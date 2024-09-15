b36df84c5c5a0501b2338fb80dfb9c97c26ceef0
windows10 <windows10@windows10.jj>
2024-09-15 02:42:55 +0200

    Leggo#

---
+++ BBSaveOverwrite/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace BBSaveOverwrite
+{
+    partial class bbsaveoverwriteform
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(bbsaveoverwriteform));
+            sourcefoldertextbox = new TextBox();
+            sourcefolderbutton = new Button();
+            destfolderbutton = new Button();
+            destfoldertextbox = new TextBox();
+            desthotkeylabel = new Label();
+            overwritedestbutton = new Button();
+            overwritesourcebutton = new Button();
+            sourcehotkeylabel = new Label();
+            toolTip1 = new ToolTip(components);
+            sourcefolderqlabel = new Label();
+            destfolderqlabel = new Label();
+            overwritedestfolderqlabel = new Label();
+            overwritesourcefolderqlabel = new Label();
+            loadedtestbutton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            backedtestbutton = new Button();
+            SuspendLayout();
+            // 
+            // sourcefoldertextbox
+            // 
+            sourcefoldertextbox.Location = new Point(456, 198);
+            sourcefoldertextbox.Name = "sourcefoldertextbox";
+            sourcefoldertextbox.Size = new Size(1365, 55);
+            sourcefoldertextbox.TabIndex = 0;
+            // 
+            // sourcefolderbutton
+            // 
+            sourcefolderbutton.Location = new Point(123, 191);
+            sourcefolderbutton.Name = "sourcefolderbutton";
+            sourcefolderbutton.Size = new Size(312, 69);
+            sourcefolderbutton.TabIndex = 1;
+            sourcefolderbutton.Text = "Source Folder";
+            sourcefolderbutton.UseVisualStyleBackColor = true;
+            sourcefolderbutton.Click += sourcefolderbutton_Click;
+            // 
+            // destfolderbutton
+            // 
+            destfolderbutton.Location = new Point(123, 322);
+            destfolderbutton.Name = "destfolderbutton";
+            destfolderbutton.Size = new Size(312, 69);
+            destfolderbutton.TabIndex = 2;
+            destfolderbutton.Text = "Dest. Folder";
+            destfolderbutton.UseVisualStyleBackColor = true;
+            destfolderbutton.Click += destfolderbutton_Click;
+            // 
+            // destfoldertextbox
+            // 
+            destfoldertextbox.Location = new Point(456, 329);
+            destfoldertextbox.Name = "destfoldertextbox";
+            destfoldertextbox.Size = new Size(1365, 55);
+            destfoldertextbox.TabIndex = 3;
+            // 
+            // desthotkeylabel
+            // 
+            desthotkeylabel.AutoSize = true;
+            desthotkeylabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            desthotkeylabel.Location = new Point(606, 469);
+            desthotkeylabel.Name = "desthotkeylabel";
+            desthotkeylabel.Size = new Size(268, 48);
+            desthotkeylabel.TabIndex = 4;
+            desthotkeylabel.Text = "Hotkey: ALT+S";
+            // 
+            // overwritedestbutton
+            // 
+            overwritedestbutton.Location = new Point(123, 459);
+            overwritedestbutton.Name = "overwritedestbutton";
+            overwritedestbutton.Size = new Size(435, 69);
+            overwritedestbutton.TabIndex = 5;
+            overwritedestbutton.Text = "Overwrite Dest. Folder";
+            overwritedestbutton.UseVisualStyleBackColor = true;
+            overwritedestbutton.Click += overwritedestbutton_Click;
+            // 
+            // overwritesourcebutton
+            // 
+            overwritesourcebutton.Location = new Point(123, 589);
+            overwritesourcebutton.Name = "overwritesourcebutton";
+            overwritesourcebutton.Size = new Size(435, 69);
+            overwritesourcebutton.TabIndex = 6;
+            overwritesourcebutton.Text = "Overwrite Source Folder";
+            overwritesourcebutton.UseVisualStyleBackColor = true;
+            overwritesourcebutton.Click += overwritesourcebutton_Click;
+            // 
+            // sourcehotkeylabel
+            // 
+            sourcehotkeylabel.AutoSize = true;
+            sourcehotkeylabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            sourcehotkeylabel.Location = new Point(606, 599);
+            sourcehotkeylabel.Name = "sourcehotkeylabel";
+            sourcehotkeylabel.Size = new Size(392, 48);
+            sourcehotkeylabel.TabIndex = 7;
+            sourcehotkeylabel.Text = "Hotkey: ALT+SHIFT+S";
+            // 
+            // sourcefolderqlabel
+            // 
+            sourcefolderqlabel.AutoSize = true;
+            sourcefolderqlabel.BackColor = Color.Red;
+            sourcefolderqlabel.ForeColor = SystemColors.Info;
+            sourcefolderqlabel.Location = new Point(1854, 201);
+            sourcefolderqlabel.Name = "sourcefolderqlabel";
+            sourcefolderqlabel.Size = new Size(36, 48);
+            sourcefolderqlabel.TabIndex = 8;
+            sourcefolderqlabel.Text = "?";
+            toolTip1.SetToolTip(sourcefolderqlabel, "Select a '1' folder (name doesn't matter) from a shadPS4 'user/savedata' save");
+            // 
+            // destfolderqlabel
+            // 
+            destfolderqlabel.AutoSize = true;
+            destfolderqlabel.BackColor = Color.Red;
+            destfolderqlabel.ForeColor = SystemColors.Info;
+            destfolderqlabel.Location = new Point(1854, 332);
+            destfolderqlabel.Name = "destfolderqlabel";
+            destfolderqlabel.Size = new Size(36, 48);
+            destfolderqlabel.TabIndex = 9;
+            destfolderqlabel.Text = "?";
+            toolTip1.SetToolTip(destfolderqlabel, "Select your shadPS4 user/'savedata' destination folder");
+            // 
+            // overwritedestfolderqlabel
+            // 
+            overwritedestfolderqlabel.AutoSize = true;
+            overwritedestfolderqlabel.BackColor = Color.Red;
+            overwritedestfolderqlabel.ForeColor = SystemColors.Info;
+            overwritedestfolderqlabel.Location = new Point(1854, 469);
+            overwritedestfolderqlabel.Name = "overwritedestfolderqlabel";
+            overwritedestfolderqlabel.Size = new Size(36, 48);
+            overwritedestfolderqlabel.TabIndex = 10;
+            overwritedestfolderqlabel.Text = "?";
+            toolTip1.SetToolTip(overwritedestfolderqlabel, "Overwrites the Source Folder over the Destination Folder without asking.");
+            // 
+            // overwritesourcefolderqlabel
+            // 
+            overwritesourcefolderqlabel.AutoSize = true;
+            overwritesourcefolderqlabel.BackColor = Color.Red;
+            overwritesourcefolderqlabel.ForeColor = SystemColors.Info;
+            overwritesourcefolderqlabel.Location = new Point(1854, 599);
+            overwritesourcefolderqlabel.Name = "overwritesourcefolderqlabel";
+            overwritesourcefolderqlabel.Size = new Size(36, 48);
+            overwritesourcefolderqlabel.TabIndex = 11;
+            overwritesourcefolderqlabel.Text = "?";
+            toolTip1.SetToolTip(overwritesourcefolderqlabel, "Reverse Overwrites the current shadPS4 savestate to the selected Source Folder.");
+            // 
+            // loadedtestbutton
+            // 
+            loadedtestbutton.Location = new Point(1371, 468);
+            loadedtestbutton.Name = "loadedtestbutton";
+            loadedtestbutton.Size = new Size(50, 50);
+            loadedtestbutton.TabIndex = 12;
+            loadedtestbutton.Text = "L";
+            loadedtestbutton.UseVisualStyleBackColor = true;
+            loadedtestbutton.Click += loadedtestbutton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(1154, 469);
+            label1.Name = "label1";
+            label1.Size = new Size(201, 48);
+            label1.TabIndex = 13;
+            label1.Text = "Soundtest:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(1154, 599);
+            label2.Name = "label2";
+            label2.Size = new Size(201, 48);
+            label2.TabIndex = 14;
+            label2.Text = "Soundtest:";
+            // 
+            // backedtestbutton
+            // 
+            backedtestbutton.Location = new Point(1371, 597);
+            backedtestbutton.Name = "backedtestbutton";
+            backedtestbutton.Size = new Size(50, 50);
+            backedtestbutton.TabIndex = 15;
+            backedtestbutton.Text = "B";
+            backedtestbutton.UseVisualStyleBackColor = true;
+            backedtestbutton.Click += backedtestbutton_Click;
+            // 
+            // bbsaveoverwriteform
+            // 
+            AutoScaleDimensions = new SizeF(20F, 48F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1943, 862);
+            Controls.Add(backedtestbutton);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(loadedtestbutton);
+            Controls.Add(overwritesourcefolderqlabel);
+            Controls.Add(overwritedestfolderqlabel);
+            Controls.Add(destfolderqlabel);
+            Controls.Add(sourcefolderqlabel);
+            Controls.Add(sourcehotkeylabel);
+            Controls.Add(overwritesourcebutton);
+            Controls.Add(overwritedestbutton);
+            Controls.Add(desthotkeylabel);
+            Controls.Add(destfoldertextbox);
+            Controls.Add(destfolderbutton);
+            Controls.Add(sourcefolderbutton);
+            Controls.Add(sourcefoldertextbox);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "bbsaveoverwriteform";
+            Text = "BBSaveOverwrite";
+            FormClosing += bbsaveoverwriteform_FormClosing;
+            Load += bbsaveoverwriteform_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        public void bbsaveoverwriteform_Load(object sender, EventArgs e)
+        {
+            sourcefoldertextbox.Text = Properties.Settings.Default.SourceFolderSetting;
+            destfoldertextbox.Text = Properties.Settings.Default.DestFolderSetting;
+
+            HotKeyManager.RegisterHotKey(Keys.S, KeyModifiers.Alt);
+            HotKeyManager.RegisterHotKey(Keys.S, KeyModifiers.Alt | KeyModifiers.Shift);
+            HotKeyManager.HotKeyPressed += new EventHandler<HotKeyEventArgs>(HotKeyManager_HotKeyPressed);
+        }
+
+        private void bbsaveoverwriteform_FormClosing(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.SourceFolderSetting = sourcefoldertextbox.Text;
+            Properties.Settings.Default.DestFolderSetting = destfoldertextbox.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        #endregion
+
+        public TextBox sourcefoldertextbox;
+        private Button sourcefolderbutton;
+        private Button destfolderbutton;
+        private Label desthotkeylabel;
+        private Button overwritedestbutton;
+        public TextBox destfoldertextbox;
+        private Button overwritesourcebutton;
+        private Label sourcehotkeylabel;
+        private ToolTip toolTip1;
+        private Label sourcefolderqlabel;
+        private Label destfolderqlabel;
+        private Label overwritedestfolderqlabel;
+        private Label overwritesourcefolderqlabel;
+        private Button loadedtestbutton;
+        private Label label1;
+        private Label label2;
+        private Button backedtestbutton;
+    }
+}
---
+++ BBSaveOverwrite/Form1.cs
@@ -0,0 +1,272 @@
+using System.Diagnostics;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace BBSaveOverwrite
+{
+    public partial class bbsaveoverwriteform : Form
+    {
+        public bbsaveoverwriteform()
+        {
+            InitializeComponent();
+
+        }
+
+        public void playaudioloaded()
+        {
+            using (var audioStream = new MemoryStream(Properties.Resource1.loaded))
+            {
+                using (var player = new SoundPlayer(audioStream))
+                {
+                    player.Play();
+                }
+            }
+        }
+
+        public void playaudiobacked()
+        {
+            using (var audioStream = new MemoryStream(Properties.Resource1.backed))
+            {
+                using (var player = new SoundPlayer(audioStream))
+                {
+                    player.Play();
+                }
+            }
+        }
+
+        void HotKeyManager_HotKeyPressed(object sender, HotKeyEventArgs e)
+        {
+
+            if ((e.Modifiers == KeyModifiers.Alt) && (e.Key == Keys.S))
+            {
+                CopyDirectory(@sourcefoldertextbox.Text, (@destfoldertextbox.Text + "\\1"), true);
+            }
+            else if ((e.Modifiers == (KeyModifiers.Alt | KeyModifiers.Shift)) && (e.Key == Keys.S))
+            {
+                CopyDirectory((@destfoldertextbox.Text + "\\1"), @sourcefoldertextbox.Text, true);
+            }
+
+        }
+
+        private void sourcefolderbutton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog sourcefolder = new FolderBrowserDialog();
+            sourcefolder.Description = "Select a '1' folder (name doesn't matter) from a shadPS4 'user/savedata' save";
+            if (sourcefolder.ShowDialog() == DialogResult.OK)
+            {
+                sourcefoldertextbox.Text = sourcefolder.SelectedPath;
+                Properties.Settings.Default.SourceFolderSetting = sourcefolder.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+
+        }
+
+        private void destfolderbutton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog destfolder = new FolderBrowserDialog();
+            destfolder.Description = "Select your shadPS4 user/'savedata' destination folder";
+            if (destfolder.ShowDialog() == DialogResult.OK)
+            {
+                destfoldertextbox.Text = destfolder.SelectedPath;
+                Properties.Settings.Default.DestFolderSetting = destfolder.SelectedPath;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void overwritedestbutton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                CopyDirectory(@sourcefoldertextbox.Text, (@destfoldertextbox.Text + "\\1"), true);
+                playaudioloaded();
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        private void overwritesourcebutton_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                CopyDirectory((@destfoldertextbox.Text + "\\1"), @sourcefoldertextbox.Text, true);
+                playaudiobacked();
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // czech if BB save
+
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath, true);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
+
+        private void loadedtestbutton_Click(object sender, EventArgs e)
+        {
+            playaudioloaded();
+        }
+
+        private void backedtestbutton_Click(object sender, EventArgs e)
+        {
+            playaudiobacked();
+        }
+
+    }
+
+    public static class HotKeyManager
+    {
+        public static event EventHandler<HotKeyEventArgs> HotKeyPressed;
+
+        public static int RegisterHotKey(Keys key, KeyModifiers modifiers)
+        {
+            _windowReadyEvent.WaitOne();
+            int id = System.Threading.Interlocked.Increment(ref _id);
+            _wnd.Invoke(new RegisterHotKeyDelegate(RegisterHotKeyInternal), _hwnd, id, (uint)modifiers, (uint)key);
+            return id;
+        }
+
+        public static void UnregisterHotKey(int id)
+        {
+            _wnd.Invoke(new UnRegisterHotKeyDelegate(UnRegisterHotKeyInternal), _hwnd, id);
+        }
+
+        delegate void RegisterHotKeyDelegate(IntPtr hwnd, int id, uint modifiers, uint key);
+        delegate void UnRegisterHotKeyDelegate(IntPtr hwnd, int id);
+
+        private static void RegisterHotKeyInternal(IntPtr hwnd, int id, uint modifiers, uint key)
+        {
+            RegisterHotKey(hwnd, id, modifiers, key);
+        }
+
+        private static void UnRegisterHotKeyInternal(IntPtr hwnd, int id)
+        {
+            UnregisterHotKey(_hwnd, id);
+        }
+
+        private static void OnHotKeyPressed(HotKeyEventArgs e)
+        {
+            if (HotKeyManager.HotKeyPressed != null)
+            {
+                HotKeyManager.HotKeyPressed(null, e);
+            }
+        }
+
+        private static volatile MessageWindow _wnd;
+        private static volatile IntPtr _hwnd;
+        private static ManualResetEvent _windowReadyEvent = new ManualResetEvent(false);
+        static HotKeyManager()
+        {
+            Thread messageLoop = new Thread(delegate ()
+            {
+                Application.Run(new MessageWindow());
+            });
+            messageLoop.Name = "MessageLoopThread";
+            messageLoop.IsBackground = true;
+            messageLoop.Start();
+        }
+
+        private class MessageWindow : Form
+        {
+            public MessageWindow()
+            {
+                _wnd = this;
+                _hwnd = this.Handle;
+                _windowReadyEvent.Set();
+            }
+
+            protected override void WndProc(ref Message m)
+            {
+                if (m.Msg == WM_HOTKEY)
+                {
+                    HotKeyEventArgs e = new HotKeyEventArgs(m.LParam);
+                    HotKeyManager.OnHotKeyPressed(e);
+                }
+
+                base.WndProc(ref m);
+            }
+
+            protected override void SetVisibleCore(bool value)
+            {
+                // Ensure the window never becomes visible
+                base.SetVisibleCore(false);
+            }
+
+            private const int WM_HOTKEY = 0x312;
+        }
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private static int _id = 0;
+    }
+
+    public class HotKeyEventArgs : EventArgs
+    {
+        public readonly Keys Key;
+        public readonly KeyModifiers Modifiers;
+
+        public HotKeyEventArgs(Keys key, KeyModifiers modifiers)
+        {
+            this.Key = key;
+            this.Modifiers = modifiers;
+        }
+
+        public HotKeyEventArgs(IntPtr hotKeyParam)
+        {
+            uint param = (uint)hotKeyParam.ToInt64();
+            Key = (Keys)((param & 0xffff0000) >> 16);
+            Modifiers = (KeyModifiers)(param & 0x0000ffff);
+        }
+    }
+
+    [Flags]
+    public enum KeyModifiers
+    {
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Windows = 8,
+        NoRepeat = 0x4000
+    }
+
+}
